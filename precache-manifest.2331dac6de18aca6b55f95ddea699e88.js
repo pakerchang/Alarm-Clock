955efcb1e15d394bc92a91456e7ba7f492c62e23
pakerchang <pakerchang@gmail.com>
2020-09-20 07:25:54 +0800

    Updates

---
+++ precache-manifest.2331dac6de18aca6b55f95ddea699e88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5dc13e53580ee9f085157090f9dc94",
+    "url": "/alarm-clock/index.html"
+  },
+  {
+    "revision": "de1f191119ccc6b0925a",
+    "url": "/alarm-clock/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "19c1cdadefcb8c817e11",
+    "url": "/alarm-clock/static/css/main.58f18bd9.chunk.css"
+  },
+  {
+    "revision": "de1f191119ccc6b0925a",
+    "url": "/alarm-clock/static/js/2.42a81ab4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/alarm-clock/static/js/2.42a81ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c1cdadefcb8c817e11",
+    "url": "/alarm-clock/static/js/main.8a1fa0ad.chunk.js"
+  },
+  {
+    "revision": "a7a72e17d2a815e984b7",
+    "url": "/alarm-clock/static/js/runtime-main.cbd40c17.js"
+  },
+  {
+    "revision": "c4362929383d28717bfb1c66c330e2f0",
+    "url": "/alarm-clock/static/media/Alarm-Clock-Sound.c4362929.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alarm-clock-app/precache-manifest.73137f9c3e58f90a424bab4fab793723.js"
+  "/alarm-clock/precache-manifest.2331dac6de18aca6b55f95ddea699e88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alarm-clock-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alarm-clock/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
